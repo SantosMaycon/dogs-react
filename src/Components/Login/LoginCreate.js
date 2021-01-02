32693af56811f87ae43c4682d431e903fa7d2eb6
SantosMaycon <mayconalves.santos14@gmail.com>
2021-01-02 05:56:30 -0300

    add useFetch

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { USER_POST } from '../../api'
 import '../../App.css'
+import useFetch from '../../Hooks/useFetch'
 import useForm from '../../Hooks/useForm'
 import { UserContext } from '../../UserContext'
 import Button from '../Forms/Button'
@@ -13,24 +14,25 @@ const LoginCreate = () => {
   const password = useForm()
 
   const { userLogin } = React.useContext(UserContext)
+  const { loading, error, request } = useFetch()
 
   async function handleSubmit(event) {
+    event.preventDefault()
+   
     const body = {
       username: username.value,
       email: email.value,
       password: password.value
     }
 
-    console.log(body)
-
-    event.preventDefault()
     if ( username.validate() && email.validate() && password.validate()) {
       const { url, options } = USER_POST(body)
 
-      const response = await fetch(url, options)
-      const json = await response.json()
+      const { response, json } = await request(url, options)
+      //const json = await response.json()
+      console.log(response)
       console.log(json)
-      await userLogin(username.value, password.value)
+      if (response.ok) await userLogin(username.value, password.value)
     }
   }
   
@@ -40,9 +42,9 @@ const LoginCreate = () => {
       <form onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email}/>
-        <Input label="Senha" type="password" name="password" autocomplete="on" {...password}/>
-        <Button>Cadastrar</Button>
-        <Error />
+        <Input label="Senha" type="password" name="password" autoComplete="on" {...password}/>
+        { loading ? <Button disabled >Carregando...</Button> : <Button>Cadastrar</Button>}
+        <Error error={error}/>
       </form>
     </section>
   )
