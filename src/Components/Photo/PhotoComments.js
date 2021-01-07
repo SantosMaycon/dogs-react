e325b1f8f3fe85d82cb16a8952c6bd29a67f65a3
SantosMaycon <mayconalves.santos14@gmail.com>
2021-01-07 03:31:10 -0300

    comentário estilo

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -5,11 +5,16 @@ import styles from './PhotoComments.module.css'
 
 const PhotoComments = ({id, comments}) => {
   const [_comments, set_Comments] = React.useState(() => comments)
+  const commentsSection = React.useRef(null)
   const { login } = React.useContext(UserContext)
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight
+  },[_comments])
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul className={styles.comments} ref={commentsSection}>
         {_comments.map((comment) => <li key={comment.comment_ID}>
           <b>{comment.comment_author}: </b>
           <span>{comment.comment_content}</span>
